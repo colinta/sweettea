092d42ac6910b06907045afb89b6c36861288eca
Colin T.A. Gray <colinta@gmail.com>
2013-04-10 12:17:11 -0600

    compatibility

--- lib/sweettea/handlers.rb
+++ lib/sweettea/handlers.rb
@@ -131,6 +131,12 @@ Teacup.handler UIButton, :font { |font|
   self.titleLabel.font = font && font.uifont
 }
 
+# for backwards compatibility - Teacup defines this *without* the UIColor
+# coercion.
+Teacup.handler UIButton, :titleColor { |target, color|
+  target.setTitleColor(color.uicolor, forState: UIControlStateNormal)
+}
+
 module Sweettea
   module_function
 
