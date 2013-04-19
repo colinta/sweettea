e44c8bba2f40605629491a783ca7b6b2bb90bb7e
Colin T.A. Gray <colinta@gmail.com>
2013-04-19 11:28:17 -0600

    new styles!  and dropped the UILabel style

--- lib/sweettea/styles.rb
+++ lib/sweettea/styles.rb
@@ -1,5 +1,6 @@
 Teacup::Stylesheet.new :sweettea do
-  style :UILabel,
+
+  style :label,
     font: :system.uifont(17),
     numberOfLines: 1,
     minimumFontSize: 10,
@@ -8,6 +9,45 @@ Teacup::Stylesheet.new :sweettea do
     lineBreakMode: :tailtruncation,
     alignment: :left,
     color: :black,
+    opaque: false,
     backgroundColor: :clear
+
+  style :input,
+    font: :system.uifont(14),
+    color: :black,
+    border: :rounded,
+    alignment: :left,
+    opaque: false,
+    backgroundColor: :clear
+
+  style :name_input, extends: :input,
+    keyboardType: UIKeyboardTypeDefault,
+    autocapitalizationType: UITextAutocapitalizationTypeWords,
+    autocorrectionType: UITextAutocorrectionTypeNo,
+    spellCheckingType: UITextSpellCheckingTypeNo
+
+  style :ascii_input, extends: :input,
+    keyboardType: UIKeyboardTypeASCIICapable
+
+  style :email_input, extends: :input,
+    keyboardType: UIKeyboardTypeEmailAddress,
+    autocapitalizationType: UITextAutocapitalizationTypeNone,
+    autocorrectionType: UITextAutocorrectionTypeNo,
+    spellCheckingType: UITextSpellCheckingTypeNo
+
+  style :url_input, extends: :input,
+    keyboardType: UIKeyboardTypeURL,
+    autocapitalizationType: UITextAutocapitalizationTypeNone,
+    autocorrectionType: UITextAutocorrectionTypeNo,
+    spellCheckingType: UITextSpellCheckingTypeNo
+
+  style :number_input, extends: :input,
+    keyboardType: UIKeyboardTypeNumberPad
+
+  style :phone_input, extends: :input,
+    keyboardType: UIKeyboardTypePhonePad
+
+  style :secure_input, extends: :input,
+    secureTextEntry: true
 
 end
